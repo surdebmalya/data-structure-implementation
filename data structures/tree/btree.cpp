803c258c2778b080a2f2c1d92915bf8f2aef8ae8
Debmalya Sur <surdebmalya2001@gmail.com>
2022-04-09 15:24:44 +0530

    tree functions added

--- data structures/tree/btree.cpp
+++ data structures/tree/btree.cpp
@@ -30,6 +30,17 @@ void inorder_traversal(struct Node * root) {
     }
 }
 
+void preorder_traversal(struct Node * root) {
+    if (root!=NULL) {
+        cout << root->value << " ";
+        preorder_traversal(root->left);
+        preorder_traversal(root->right);
+    }
+    else {
+        return;
+    }
+}
+
 int height(struct Node * root) {
     if (root==NULL) {
         return 0;
@@ -56,6 +67,36 @@ int number_of_leaf_nodes(struct Node * root) {
     }
 }
 
+void mirror(struct Node * root) {
+    if (root==NULL) {
+        return;
+    }
+    else {
+        if (root->left!=NULL and root->right!=NULL) {
+            mirror(root->left);
+            mirror(root->right);
+            struct Node * temp_left = root->left;
+            struct Node * temp_right = root->right;
+            root->left = temp_right;
+            root->right = temp_left;
+        }
+        else if (root->left!=NULL and root->right==NULL) {
+            mirror(root->left);
+            root->right = root->left;
+            root->left = NULL;
+        }
+        else if (root->left==NULL and root->right!=NULL) {
+            mirror(root->right);
+            root->left = root->right;
+            root->right = NULL;
+        }
+        else {
+            // it's a leaf node
+            return;
+        }
+    }
+}
+
 int main() {
     struct Node * root = newNode(1);
     root->left = newNode(2);
@@ -69,6 +110,16 @@ int main() {
     cout << "Height of the Tree: " << height(root) << endl;
 
     cout << "Number of leaf nodes: " << number_of_leaf_nodes(root) << endl;
+
+    cout << "Mirror of current tree" << endl;
+
+    mirror(root);
+    cout << "Inorder Traversal: ";
+    inorder_traversal(root);
+    cout << endl;
+    cout << "Preorder Traversal: ";
+    preorder_traversal(root);
+    cout << endl;
 
     return 0;
 }
