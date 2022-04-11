ee7250571687954ad0c6ccd1517541aa96462dde
Debmalya Sur <surdebmalya2001@gmail.com>
2022-04-11 11:48:52 +0530

    sort added

--- data structures/sorting/sort.cpp
+++ data structures/sorting/sort.cpp
@@ -1,10 +1,34 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
+template<typename T>
+void print_container(T container) {
+    auto it = container.begin();
+    while (it != container.end()) {
+        cout << *it << " ";
+        it++;
+    }
+    cout << endl;
+}
+
 int main() {
+    vector<int> numbers = {2, 4, 5, 10, 6, 73, 8, 1, 9};
+    // numbers.erase(numbers.begin()+2, numbers.begin()+5); // erase all elements from 3rd to 5th element
+    print_container(numbers);
+    sort(numbers.begin(), numbers.end());
+    print_container(numbers);
+    // sort decending order
+    sort(numbers.begin(), numbers.end(), greater<int> ());
+    print_container(numbers);
+
+    vector<char> characters = {'g', 'b', 'y', 'a', 't', 'm', 'f', 'z', 'c'};
+    print_container(characters);
+    // partial sort
+    sort(characters.begin(), characters.begin()+3);
+    print_container(characters);
 
     return 0;
 }
