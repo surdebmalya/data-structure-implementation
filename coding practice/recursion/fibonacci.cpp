fb31fc82d31ecfdad501b212ed9049ac276d125d
Debmalya Sur <surdebmalya2001@gmail.com>
2022-04-30 20:34:02 +0530

    fibonacci problem added

---
+++ coding practice/recursion/fibonacci.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+// Easy
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 or n == 1) {
+            return n;
+        }
+        vector<int> fibbo;
+        fibbo.push_back(0);
+        fibbo.push_back(1);
+        for (int i = 2; i <= n; i++) {
+            int temp = fibbo[i - 1] + fibbo[i - 2];
+            fibbo.push_back(temp);
+        }
+        return fibbo[n];
+    }
+};
