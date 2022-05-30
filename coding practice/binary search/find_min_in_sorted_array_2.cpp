f47591c0ec45a726dcdaf314e6314f132365753e
Debmalya Sur <surdebmalya2001@gmail.com>
2022-05-30 19:58:40 +0530

    find minimum number in sorted array 2 added

---
+++ coding practice/binary search/find_min_in_sorted_array_2.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+// Hard
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0, r=nums.size()-1; 
+        while(l<r) {
+            int mid=(l+r)/2;
+            if(nums[mid]>nums[r]) l=mid+1;
+            else {
+                if(nums[mid]!=nums[r]) r=mid;
+                else r=r-1;
+            }
+        }
+        return nums[l];
+    }
+};
