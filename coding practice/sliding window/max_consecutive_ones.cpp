9a83c27d45d48585d72259161b0350d956a7e2e1
Debmalya Sur <surdebmalya2001@gmail.com>
2022-06-07 17:50:14 +0530

    max consecutive ones 3 added

---
+++ coding practice/sliding window/max_consecutive_ones.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+
+// Medium
+
+class Solution {
+public:
+    int longestOnes(vector<int> &nums, int k) {
+        int i = 0, j = 0;
+        while (j < nums.size()) {
+            if (nums[j] == 0)
+                k--;
+            if (k < 0) {
+                if (nums[i] == 0)
+                    k++;
+                i++;
+            }
+            j++;
+        }
+        return j - i;
+    }
+};
