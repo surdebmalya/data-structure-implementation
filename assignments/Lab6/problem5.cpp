d67ef8f95c91d42e9c102e454d32b418ab0bf007
Debmalya Sur <surdebmalya2001@gmail.com>
2022-09-12 14:39:05 +0530

    lab assignment 6 code files updated

--- assignments/Lab6/problem5.cpp
+++ assignments/Lab6/problem5.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
-	    // Code here
-	    vector<int>dist(n,INT_MAX);
-    dist[0]=0;
-    for(int i=1;i<=n-1;i++){
-        for(auto edge:edges){
-            if(dist[edge[0]+edge[2]<dist[edge[1]]]){
-                dist[edge[1]]=dist[edge[0]]+edge[2];
+    int isNegativeWeightCycle(int n, vector<vector<int>> edges) {
+        vector<int> dist(n, 1e8);
+        dist[0] = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (auto v : edges) {
+                if (dist[v[1]] > dist[v[0]] + v[2]) {
+                    dist[v[1]] = dist[v[0]] + v[2];
+                }
             }
         }
-    }
-    
-        for(auto edge:edges){
-            if(dist[edge[0]]+edge[2]<dist[edge[1]]){
+        for (auto v : edges) {
+            if (dist[v[0]] + v[2] < dist[v[1]])
                 return 1;
-            }
         }
-    return 0;
-	}
+        return 0;
+    }
 };
