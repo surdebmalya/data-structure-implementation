ed69f75ad26463ffe292c01b9b4b1c3723ee7821
Debmalya Sur <surdebmalya2001@gmail.com>
2023-01-06 10:29:47 +0530

    maximum-ice-cream-bars added

--- leetcode/greedy/README.md
+++ leetcode/greedy/README.md
@@ -1,3 +1,4 @@
 earliest-possible-day-of-full-bloom
 minimum-rounds-to-complete-all-tasks
 minimum-number-of-arrows-to-burst-balloons
+maximum-ice-cream-bars
---
+++ leetcode/greedy/maximum-ice-cream-bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int res=0;
+        for(int i=0; i<costs.size(); i++) {
+            if(costs[i]<=coins) {
+                res++;
+                coins -= costs[i];
+            }
+            else break;
+        }
+        return res;
+    }
+};
